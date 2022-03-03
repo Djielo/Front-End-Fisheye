36097c689d434f1407b780ee5c4a618bcf614b2a
Jielo <84443714+Djielo@users.noreply.github.com>
2022-03-03 11:25:17 +0100

    Change fetch link 2

--- data/getDatas.js
+++ data/getDatas.js
@@ -1,6 +1,6 @@
 //Récupère les données du fichier "photographers.json"
 export const getDatas = async (array, target) => {
-  await fetch("./data/photographers.json")
+  await fetch("../../data/photographers.json")
     .then((response) => response.json())
     .then((promise) => {
       //Stocke les données dans le tableau "myData"
