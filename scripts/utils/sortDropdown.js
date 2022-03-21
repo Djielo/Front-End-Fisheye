574468a11fc96b841a7725c09d147359dae0c5ac
Djielo <business.jlo68@gmail.com>
2022-03-21 18:10:34 +0100

    Up & Down Icon on Dropdown Menu

--- scripts/utils/sortDropdown.js
+++ scripts/utils/sortDropdown.js
@@ -1,20 +1,29 @@
 export const sortDropdown = () => {
   const mouseHover = document.querySelector(".sort_dropdown");
   let menu = document.querySelectorAll(".menu");
+  let chevron = document.querySelector(".fa-chevron-down");
   mouseHover.addEventListener("mouseover", () => {
     // pour chaque élément du tableau "menu", vérifier s'il existe une classe "menu
     menu.forEach((element) => {
+      console.log(element);
       if (element.classList.contains("menuUnvisible")) {
-        element.classList.replace("menuUnvisible", "menuVisible");        
+        element.classList.replace("menuUnvisible", "menuVisible");
       }
     });
+    console.log(chevron.classList.contains("fa-chevron-down"));
+    if (chevron.classList.contains("fa-chevron-down")) {
+      chevron.classList.replace("fa-chevron-down", "fa-chevron-up");
+    }
   });
   mouseHover.addEventListener("mouseout", () => {
     // pour chaque élément du tableau "menu", vérifier s'il existe une classe "menu
     menu.forEach((element) => {
       if (element.classList.contains("menuVisible")) {
-        element.classList.replace("menuVisible", "menuUnvisible");        
+        element.classList.replace("menuVisible", "menuUnvisible");
       }
     });
+    if (chevron.classList.contains("fa-chevron-up")) {
+      chevron.classList.replace("fa-chevron-up", "fa-chevron-down");
+    }
   });
 };
