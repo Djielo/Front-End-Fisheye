945d4445d71820c451b6639551293553cc184d78
Djielo <business.jlo68@gmail.com>
2022-03-22 12:42:57 +0100

    Dropdown replace text finished

--- scripts/utils/displaySort.js
+++ scripts/utils/displaySort.js
@@ -1,28 +1,71 @@
 import { displayMediasThisPhotographer } from "./displayMediasThisPhotographer.js";
 
-export const displaySort = (array, thisPhotographer) => {  
+export const displaySort = (array, thisPhotographer) => {
   let menu = document.querySelectorAll(".menu");
+  const position1 = document.querySelector(".pos1");
+  const position2 = document.querySelector(".pos2");
+  const position3 = document.querySelector(".pos3");
+  let tempo = [];
 
-  menu.forEach((element) => {    
+  menu.forEach((element) => {
     element.addEventListener("click", () => {
+      //
+      //
+      ////////// DATE
       if (element.innerText == "Date") {
-        console.log("pos1");        
         array.sort((a, b) => {
-          return a.date < b.date ? -1 : 1;
+          return a.date > b.date ? -1 : 1;
         });
+        console.log(element.innerText);
+        if (element.innerText == "Date" && element.classList.contains("pos2")) {
+          tempo = position1.innerText;
+          position1.innerText = position2.innerText;
+          position2.innerText = tempo;
+        } else if (element.innerText == "Date" && element.classList.contains("pos3")) {
+          tempo = position1.innerText;
+          position1.innerText = position3.innerText;
+          position3.innerText = tempo;
+        }
       }
-      if (element.innerText == "Popularité") {
-        console.log("pos2");
+
+      //
+      //
+      ////////// POPULARITE
+      else if (element.innerText == "Popularité") {
         array.sort((a, b) => {
           return a.likes > b.likes ? -1 : 1;
         });
+        console.log(element.innerText);
+        if (element.innerText == "Popularité" && element.classList.contains("pos2")) {
+          tempo = position1.innerText;
+          position1.innerText = position2.innerText;
+          position2.innerText = tempo;
+        } else if (element.innerText == "Popularité" && element.classList.contains("pos3")) {
+          tempo = position1.innerText;
+          position1.innerText = position3.innerText;
+          position3.innerText = tempo;
+        }
       }
-      if (element.innerText == "Titre") {
-        console.log("pos3");
+      //
+      //
+      ////////// TITRE
+      else if (element.innerText == "Titre") {
         array.sort((a, b) => {
           return a.title < b.title ? -1 : 1;
         });
+        console.log(element.innerText);
+        if (element.innerText == "Titre" && element.classList.contains("pos2")) {
+          tempo = position1.innerText;
+          position1.innerText = position2.innerText;
+          position2.innerText = tempo;
+        } else if (element.innerText == "Titre" && element.classList.contains("pos3")) {
+          tempo = position1.innerText;
+          position1.innerText = position3.innerText;
+          position3.innerText = tempo;
+        }
       }
+      //
+      //
       displayMediasThisPhotographer(array, thisPhotographer);
     });
   });
--- scripts/utils/sortDropdown.js
+++ scripts/utils/sortDropdown.js
@@ -1,18 +1,16 @@
 export const sortDropdown = () => {
   const mouseHover = document.querySelector(".sort_dropdown");
   let menu = document.querySelectorAll(".menu");
-  let chevron = document.querySelector(".fa-chevron-down");
+  let faChevronDown = document.querySelector(".fa-chevron-down");
   mouseHover.addEventListener("mouseover", () => {
     // pour chaque élément du tableau "menu", vérifier s'il existe une classe "menu
     menu.forEach((element) => {
-      console.log(element);
       if (element.classList.contains("menuUnvisible")) {
         element.classList.replace("menuUnvisible", "menuVisible");
       }
     });
-    console.log(chevron.classList.contains("fa-chevron-down"));
-    if (chevron.classList.contains("fa-chevron-down")) {
-      chevron.classList.replace("fa-chevron-down", "fa-chevron-up");
+    if (faChevronDown.classList.contains("fa-chevron-down")) {
+      faChevronDown.classList.replace("fa-chevron-down", "fa-chevron-up");
     }
   });
   mouseHover.addEventListener("mouseout", () => {
@@ -22,8 +20,8 @@ export const sortDropdown = () => {
         element.classList.replace("menuVisible", "menuUnvisible");
       }
     });
-    if (chevron.classList.contains("fa-chevron-up")) {
-      chevron.classList.replace("fa-chevron-up", "fa-chevron-down");
+    if (faChevronDown.classList.contains("fa-chevron-up")) {
+      faChevronDown.classList.replace("fa-chevron-up", "fa-chevron-down");
     }
   });
 };
